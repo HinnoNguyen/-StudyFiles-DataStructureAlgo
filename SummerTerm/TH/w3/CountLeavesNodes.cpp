df9f94c2236ed3a8f5acdab9dcc6a4f40f6baa40
NguyenLanHuong0588 <23520588@gm.uit.edu.vn>
2025-07-23 08:37:42 +0700

    wake up buddy

---
+++ SummerTerm/TH/w3/CountLeavesNodes.cpp
@@ -0,0 +1,48 @@
+// Template
+#include <iostream>
+using namespace std;
+
+// Main code (Submitted Codes)
+struct node {
+    int data;
+    struct node* left;
+    struct node* right;
+};
+// #define node* NODE;
+
+node* NewNode(int x) {
+    node* p = new node;
+    p->data = x;
+    p->left = p->right = NULL;
+    return p;
+}
+
+bool InsertNode (node* &root, int x) {
+    if (!root) {
+        root = NewNode(x);
+        return true;
+    }
+    if (root->data > x) return InsertNode(root->left, x);
+    else if (root->data < x) return InsertNode(root->right, x);
+    return 0;
+}
+
+int CountLeaves (node* root) {
+    if (!root == NULL) return 0;
+    if (root->left == NULL && root->right == NULL) return 1;
+    return CountLeaves(root->left) + CountLeaves(root->right);
+}
+
+int main () {
+    node* root = NULL;
+    int x;
+    while (cin >> x && x != 0) InsertNode(root, x);
+    cout << CountLeaves(root);
+    return 0;
+}
+
+
+// Source
+// https://handbook.bhtcnpm.com/docs/dsa/phan2/chuong7
+
+// Check in 
