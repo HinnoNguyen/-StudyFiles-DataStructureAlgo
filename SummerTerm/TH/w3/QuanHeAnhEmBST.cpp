bbfaff983cefc76f9e95c29bf37dcd6fba0ea929
NguyenLanHuong0588 <23520588@gm.uit.edu.vn>
2025-07-30 10:25:40 +0700

    e

--- SummerTerm/TH/w3/QuanHeAnhEmBST.cpp
+++ SummerTerm/TH/w3/QuanHeAnhEmBST.cpp
@@ -33,7 +33,69 @@ bool isBrother(Tree, int, int);
 void run(int q, Tree T){
 
 // Submited Area **************************************************
+    while (q--) {
+        int u, v;
+        cin >> u >> v;
+        cout << u << " va " << v << " ";
+        if (isBrother(T, u, v))
+            cout << "la anh em\n";
+        else
+            cout << "khong phai la anh em\n";
+    }
+}
 
+void Insert(Tree &T, int x) {
+    TNode* newNode = CreateTNode(x);
+    if (T.root == NULL) {
+        T.root = newNode;
+        return;
+    }
+
+    TNode* cur = T.root;
+    TNode* prev = NULL;
+
+    while (cur != NULL) {
+        prev = cur;
+        if (x < cur->key)
+            cur = cur->pLeft;
+        else
+            cur = cur->pRight;
+    }
+
+    if (x < prev->key)
+        prev->pLeft = newNode;
+    else
+        prev->pRight = newNode;
+}
+
+bool isBrother(Tree T, int a, int b) {
+    if (a == b) return false;
+
+    TNode* cur = T.root;
+
+    while (cur != NULL) {
+        bool leftHasA = (cur->pLeft != NULL && cur->pLeft->key == a);
+        bool rightHasB = (cur->pRight != NULL && cur->pRight->key == b);
+
+        bool leftHasB = (cur->pLeft != NULL && cur->pLeft->key == b);
+        bool rightHasA = (cur->pRight != NULL && cur->pRight->key == a);
+
+        if ((leftHasA && rightHasB) || (leftHasB && rightHasA)) {
+            return true;
+        }
+
+        // Di chuyển xuống cây
+        if (a < cur->key && b < cur->key)
+            cur = cur->pLeft;
+        else if (a > cur->key && b > cur->key)
+            cur = cur->pRight;
+        else
+            cur = (cur->pLeft != NULL) ? cur->pLeft : cur->pRight;
+    }
+
+    return false;
+
+// Source: https://chatgpt.com/share/68899082-7cfc-8001-ae6a-40c02e90e7cb
 
 // Fooster
 }
