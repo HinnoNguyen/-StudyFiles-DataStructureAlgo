e51643f9a19299e941e8166cab1ec9baf1a775d4
NguyenLanHuong0588 <23520588@gm.uit.edu.vn>
2025-08-10 17:04:48 +0700

    .

---
+++ SummerTerm/CK/TH/Sort.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector <int> a(n);
+    
+    for (int i = 0; i < n; i++) cin >> a[i];
+    return 0;
+}
+
+
+int swap (int a, int b) {
+    int temp = a;
+    a = b;
+    b = temp;
+    return a;
+}
+
+void SelectionSort(vector<int> a) {
+    int n = a.size();
+    for (int i = 0; i < n - 1; i++) {
+        int temp = i;
+        for (int j = i + 1; j < n; j++) {
+            if ( a[j] < a[temp]) temp = j;
+        }
+        swap(a[i], a[temp]);
+    }
+}
+
+
+void InsertionSort(vector<int> a) {
+    int n = a.size();
+    for (int i = 1; i < n; i++) {
+        int key = a[i];
+        int j = i - 1;
+        while (j >= 0 && a[j] > key) {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = key;
+    }
+}
+
+
+void BubbleSort(vector<int> a) {
+    int n = a.size();
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (a[j] > a[j + 1]) swap(a[j], a[j + 1]);
+        }
+    }
+}
+
+
+int Partition(vector<int> &a, int low, int high) {
+    int pivot = a[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+        if (a[j] < pivot) {
+            i++;
+            swap(a[i], a[j]);
+        }
+    }
+    swap(a[i + 1], a[high]);
+    return i + 1;
+}
+
+void QuickSort(vector<int> &a, int low, int high) {
+    if (low < high) {
+        int pi = Partition(a, low, high);
+        QuickSort(a, low, pi - 1);
+        QuickSort(a, pi + 1, high);
+    }
+}
+
+void Heapify(vector<int> &a, int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && a[left] > a[largest]) largest = left;
+    if (right < n && a[right] > a[largest]) largest = right;
+
+    if (largest != i) {
+        swap(a[i], a[largest]);
+        Heapify(a, n, largest);
+    }
+}
+
+void HeapSort(vector<int> &a) {
+    int n = a.size();
+    for (int i = n / 2 - 1; i >= 0; i--) Heapify(a, n, i);
+    
+    for (int i = n - 1; i > 0; i--) {
+        swap(a[0], a[i]);
+        Heapify(a, i, 0);
+    }
+}
+
+
+void Merge(vector<int> &a, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    vector<int> L(n1), R(n2);
+    for (int i = 0; i < n1; i++) L[i] = a[left + i];
+    for (int j = 0; j < n2; j++) R[j] = a[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) a[k++] = L[i++];
+        else a[k++] = R[j++];
+    }
+    while (i < n1) a[k++] = L[i++];
+    while (j < n2) a[k++] = R[j++];
+}
+
+void MergeSort(vector<int> &a, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        MergeSort(a, left, mid);
+        MergeSort(a, mid + 1, right);
+        Merge(a, left, mid, right);
+    }
+}
